695ae2ab8247df2c3d2f50a4edaa61ade9f3ecbd
@Anna-Connors <connora2@gmatc.matc.edu>
2024-10-04 20:25:22 -0500

    Completed Activity 5

---
+++ Domain/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain {
+
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+        
+        public DateTime Date { get; set; }
+    }
+}
