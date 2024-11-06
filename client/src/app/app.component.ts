14cd8a5d63b2e7715dbdf94780e1da9b06536fec
@Anna-Connors <connora2@gmatc.matc.edu>
2024-11-06 13:14:01 -0600

    updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,8 +10,6 @@ import { MatButtonModule } from '@angular/material/button';
   standalone: true,
   imports: [
     RouterOutlet, 
-    CommonModule, 
-    MatCardModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule
@@ -22,19 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'client';
-  posts: any;
-
-  constructor(private http: HttpClient) {
-
-  }
-
-  ngOnInit(): void {
-    this.http.get('http://localhost:5020/api/posts').subscribe({
-      next: (response) => this.posts = response,
-      error: (e) => console.error(e),
-      complete: () => console.log('complete')
-    })
-  }
+export class AppComponent {
+  title = 'BlogBox';
+  
 }
